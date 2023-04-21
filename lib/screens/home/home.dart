619e475a1a5722cf7757ae7bf0c40c943426ca1a
FooSzeJie <D200234C@sc.edu.my>
2023-04-21 12:04:51 +0800

    delete floating button

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,61 +7,40 @@ import 'package:job_searching_app/screens/home/widgets/tag_list.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Stack(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(),
+              ),
+
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.grey.withOpacity(0.1),
+                ),
+              ),
+            ],
+          ),
+
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Container(),
-                    ),
-
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.grey.withOpacity(0.1),
-                      ),
-                    ),
-                  ],
-                ),
-
-                SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      HomeAppBar(),
-                      SearchCard(),
-                      TagList(),
-                      JobList(),
-                    ],
-                  ),
-                ),
+                HomeAppBar(),
+                SearchCard(),
+                TagList(),
+                JobList(),
               ],
             ),
-
-
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      // floatingActionButton: FloatingActionButton(
-      //   backgroundColor: Theme.of(context).accentColor,
-      //   onPressed: () {},
-      //   child: Icon(
-      //     Icons.add,
-      //     color: Colors.white,
-      //   ),
-      // ),
-      // bottomNavigationBar: Theme(
-      //   data: ThemeData(
-      //     splashColor: Colors.transparent,
-      //     highlightColor: Colors.transparent,
-      //   ),
-      // ),
+          ),
+        ],
+      ),
     );
-
   }
 }
